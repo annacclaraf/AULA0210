b2ccabca598b75bb4fba7eb4ead014a56f6d0f9f
annacclaraf <anna.bento@estudante.iftm.edu.br>
2025-10-02 15:35:23 -0300

    Add files via upload

---
+++ quest4,5.js
@@ -0,0 +1,18 @@
+const computador ={
+    marca: "Lenovo",
+    processador: "intel i3",
+    memoria: "8GB",
+    HD: "1TB"
+
+}
+
+const computador = {
+
+}
+
+function criarNO(){
+
+    const elementoMarca = document.createElement("h1")
+    elementoMarca.innerText =`${computador.marca}`
+    const elementoProc = document.createElement("h2")
+}
